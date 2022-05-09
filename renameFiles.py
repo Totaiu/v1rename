1e34f327296c58665ee49bca3dd7975f2a6a3bf5
vasily <wintex91@gmail.com>
2022-05-09 18:51:40 +0300

    Some changes
    for udobstvo

--- renameFiles.py
+++ renameFiles.py
@@ -3,12 +3,18 @@ import fnmatch
 
 folderPath = r'' #путь к папке
 seriesNumber = 1  #с какого числа начинаются серии
+seriesFormat = '.mkv'
+seriesFormatSearch = '*' + seriesFormat
+
 subtitlesNumber = 1 # с какого числа начинаются субтитры
+subtitleFormat = '.ass'
+subtibleFormatSearch = '*' + subtitleFormat
+
 
 for filename in os.listdir(folderPath):
-    if fnmatch.fnmatch(filename, '*.ass'):
-      os.rename(folderPath + '\\' + filename, folderPath + '\\' + str(subtitlesNumber) + '.ass')
+    if fnmatch.fnmatch(filename, subtibleFormatSearch):
+      os.rename(folderPath + '\\' + filename, folderPath + '\\' + str(subtitlesNumber) + subtitleFormat)
       subtitlesNumber +=1
-    if fnmatch.fnmatch(filename, '*.mkv'):
-      os.rename(folderPath + '\\' + filename, folderPath + '\\' + str(seriesNumber) + '.mkv')
+    if fnmatch.fnmatch(filename, seriesFormatSearch):
+      os.rename(folderPath + '\\' + filename, folderPath + '\\' + str(seriesNumber) + seriesFormat)
       seriesNumber +=1
